cf40209e3e4ce8ec8e2e5b8515cfeced1990a3e2
Jhonsferg <jhonsferg@users.noreply.github.com>
2026-03-25 17:34:50 -0500

    chore: add commitlint conventional commits config

---
+++ commitlint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('@commitlint/types').UserConfig} */
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [1, 'always', 120],
+    'footer-max-line-length': [1, 'always', 120],
+    'header-max-length': [2, 'always', 100],
+  },
+};
